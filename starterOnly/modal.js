9c2a7a427b823352a7204c0f6b429335e3c112cd
Willem Jouret <willem.jouret9@gmail.com>
2022-04-10 18:19:16 +0200

    1er essais JS (voir commentaire)

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,11 +7,19 @@ function editNav() {
   }
 }
 
+
+
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelector(".close");
+const first = document.getElementById("first");
+const last = document.getElementById("last");
+const email = document.getElementById("email");
+
+let error;
+let emailValidation = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -28,5 +36,35 @@ function launchModal() {
 function closeModal(){
   modalbg.style.display = "none";
 }
+
+// conditions champs vides 
+
+if (!first.value ){
+  error = "Veuillez renseigner un prénom"
+}
+if (!last.value ){
+  error = "Veuillez renseigner un nom"
+}
+
+// condition email invalide 
+
+  if (emailValidation.test(email) == false){
+  error = "Veuillez renseigner une adresse email valide"
+  }
+
+
+// condition affichage erreur
+
+// fonction soumettre formulaire 
+function validate(){
+  if (document.first.
+    
+    value!=""){
+    alert("Merci ! Votre réservation à été reçue.");
+    return true;
+  }
+}
+
+
 
 
