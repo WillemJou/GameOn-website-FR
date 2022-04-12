4b63e6006f82a160ad4784edc0355864b385e52a
Willem Jouret <willem.jouret9@gmail.com>
2022-04-12 17:14:54 +0200

    ajout variables (const,let)+fonctions messages erreurs+fonction validate (essais)

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,60 +10,152 @@ function editNav() {
 
 
 // DOM Elements
+
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.querySelector(".close");
+let error;
+let dataError = document.getElementsByClassName("dataError");
 const first = document.getElementById("first");
 const last = document.getElementById("last");
 const email = document.getElementById("email");
+const form = document.querySelector("reserve");
+const birthdate = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
+const checkbox = document.getElementById("checkbox1");
+const locationPlace = document.getElementsByTagName("location","location2","location3","location4","location5","location6");
+let emailValidation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+let locationArray = ["location","location2","location3","location4","location5","location6"];
 
-let error;
-let emailValidation = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+
 
 // launch modal event
+
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // close modal event
+
 closeBtn.addEventListener("click", closeModal);
 
 // launch modal form
+
 function launchModal() {
   modalbg.style.display = "block";
 }
 
 // close modal form
+
 function closeModal(){
   modalbg.style.display = "none";
 }
 
-// conditions champs vides 
+//fonction prénom+message d'erreur
 
-if (!first.value ){
-  error = "Veuillez renseigner un prénom"
+function firstCondition(a){
+  if (first.value==""){
+  
+    alert("Veuillez renseigner un prénom");
+    return false;}
+
+  else if (first.length<"2"){
+    alert("Veuillez entrer 2 caractères ou plus pour le champ de prénom");
+    return false;}
+
+  else {
+    return true;}  
 }
-if (!last.value ){
-  error = "Veuillez renseigner un nom"
-}
 
-// condition email invalide 
+//fonction nom+message d'erreur
 
+function lastCondition(b){
+  if(last.length<"2"){
+    alert("Veuillez entrer 2 caractères ou plus pour le champ de nom"); 
+    return false;}
+  
+  else if (last==""){
+      alert("Veuillez renseigner un nom");
+      return false;}
+  
+  else {
+    return true;}    
+}   
+ 
+//fonction email+message d'erreur
+
+function emailCondition(c){
   if (emailValidation.test(email) == false){
-  error = "Veuillez renseigner une adresse email valide"
-  }
+    alert("Veuillez renseigner une adresse email valide");
+    return false;}
 
-
-// condition affichage erreur
-
-// fonction soumettre formulaire 
-function validate(){
-  if (document.first.
-    
-    value!=""){
-    alert("Merci ! Votre réservation à été reçue.");
-    return true;
-  }
+  else {
+    return true;}
 }
+ 
+//condition date+message d'erreur    
+
+if (birthdate=="") {
+  alert("Vous devez entrer votre date de naissance");
+}
+
+
+//condition tournoi+message d'erreur
+
+if (quantity==""){
+  alert("Vous devez entrer un nombre");  
+}
+
+//condition location+message d'erreur
+
+if (locationArray=""){
+  alert("Vous devez choisir une ville");
+}
+
+//condition checkbox+message d'erreur
+
+if (checkbox1.checked==false){
+  alert("Vous devez vérifier que vous acceptez les termes et conditions.");}
+ 
+
+// fonction affichage boite message erreur
+
+function revealErrorContainer(r){
+  if (a, b, c=false){ 
+    dataError = true;}
+}
+
+// fonction validate sur Onsubmit
+
+function validate(){
+  if(a, b, c=true){
+    return true;}
+}
+
+
+
+   
+
+  
+
+
+
+
+ 
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
 
 
 
